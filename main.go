8f13c18f5d70b0e89af7b04e4f453b4171947041
Shivank Sharma <shivanksharma070@gmail.com>
2025-08-22 00:26:38 +0530

    Read lines from a file.
    Implement a go-routine that pushes lines read from a file into a channel

--- main.go
+++ main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer f.Close()
+		defer close(out)
+
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
+
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i+1:]
+				out <- str
+				str = ""
+			}
+
+			str += string(data)
+		}
+
+		if len(str) != 0 {
+			out <- str
+		}
+	}()
+	return out
+}
 
 func main() {
-	fmt.Println("I hope I get the job done.")
+	f, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatal("erorr", "error", err)
+	}
+
+	lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
+	}
+
 }
