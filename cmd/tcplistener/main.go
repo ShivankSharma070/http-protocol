1fc8c3692971ec194bad60724ffe071a4dcc318c
Shivank Sharma <shivanksharma070@gmail.com>
2025-09-06 13:31:13 +0530

    Parse Field-Lines
    Implement parsing of headers (field-lines)

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,46 +1,46 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"log"
 	"net"
+
+	"github.com/ShivankSharma070/http-protocol/internals/request"
 )
 
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	out := make(chan string, 1)
-
-	go func() {
-		defer f.Close()
-		defer close(out) // Closing the channel is important. Otherwise deadlock
-
-		str := ""
-		for {
-			data := make([]byte, 8)
-			n, err := f.Read(data)
-			if err != nil {
-				break
-			}
-
-			data = data[:n]
-			if i := bytes.IndexByte(data, '\n'); i != -1 {
-				str += string(data[:i])
-				data = data[i+1:]
-				out <- str
-				str = ""
-			}
-
-			str += string(data)
-		}
-
-		if len(str) != 0 {
-			out <- str
-		}
-
-	}()
-	return out
-}
+// func getLinesChannel(f io.ReadCloser) <-chan string {
+// 	out := make(chan string, 1)
+//
+// 	go func() {
+// 		defer f.Close()
+// 		defer close(out) // Closing the channel is important. Otherwise deadlock
+//
+// 		str := ""
+// 		for {
+// 			data := make([]byte, 8)
+// 			n, err := f.Read(data)
+// 			if err != nil {
+// 				break
+// 			}
+//
+// 			data = data[:n]
+// 			if i := bytes.IndexByte(data, '\n'); i != -1 {
+// 				str += string(data[:i])
+// 				data = data[i+1:]
+// 				out <- str
+// 				str = ""
+// 			}
+//
+// 			str += string(data)
+// 		}
+//
+// 		if len(str) != 0 {
+// 			out <- str
+// 		}
+//
+// 	}()
+// 	return out
+// }
 
 func main() {
 	// Start a tcp connection
@@ -56,9 +56,17 @@ func main() {
 			break
 		}
 
-		for line := range getLinesChannel(conn) {
-			fmt.Printf("read: %s\n", line)
+		re, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatal("error", "error", err)
+			break
 		}
+
+		fmt.Println("Request line:")
+		fmt.Println("- Method: ", re.RequestLine.Method)
+		fmt.Println("- Target: ", re.RequestLine.RequestTarget)
+		fmt.Println("- Version: ", re.RequestLine.HttpVersion)
+
 	}
 
 	listener.Close()
---
+++ internals/headers/headers_test.go
@@ -0,0 +1,49 @@
+package headers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHeadersParsing(t *testing.T) {
+	// Test: Valid single header
+	headers := NewHeaders()
+	data := []byte("Host: localhost:42069\r\nfoofoo:        barbar  \r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers.Get("host"))
+	assert.Equal(t, "barbar", headers.Get("foofoo"))
+	assert.Equal(t, "", headers.Get("MissingKey"))
+	assert.Equal(t, 50, n)
+	assert.True(t, done)
+
+	// Test: Invalid spacing header
+	headers = NewHeaders()
+	data = []byte("       Host : localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Test: Invalid header name
+	headers = NewHeaders()
+	data = []byte("H©st: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Test: Multiple values for a single header
+	headers = NewHeaders()
+	data = []byte("Host: localhost:42069\r\nhost: address.com\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069, address.com", headers.Get("host"))
+	assert.Equal(t, 44, n)
+	assert.True(t, done)
+
+}
